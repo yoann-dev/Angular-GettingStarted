44d10136522f4bed66bf442499271f73c06516c0
yoann-dev <yoann_dev@net-c.com>
2018-10-14 19:28:26 +0200

    Finalize NestedComponents

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { StartComponent } from './shared/star.component';
+
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
+    StartComponent,
     ProductListComponent,
     ConvertToSpacesPipe
   ],
